1cdfe1d63fc70a877bbc5d184dad2283e551d9a9
tat131313 <tatyapleshka@gmail.com>
2018-04-30 23:37:39 +0300

    decoder

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-
+$letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    
     function encoder($text, $key){
-        $letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+        global $letters;
         $text = strtolower($text);                          // text to lower                    
         $text_arr = str_split($text);                       //string -> array
         for($i = 0, $count = count($text_arr); $i < $count; $i++){
@@ -23,8 +24,7 @@
         return $encoderText;
     }
     function encoder_word($text, $key_word){
-        $letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-       
+        global $letters;
         $key_word = strtolower($key_word);                  // text to lower  
         $key_word = str_split($key_word);                   //string -> array
 
@@ -59,15 +59,87 @@
             }
         }
         return $encoder_wordText;
-    }      
+    }
     
-    include('view.php');
+    function decoder($text, $key){
+        global $letters;
+        $text = strtolower($text);                          // text to lower                    
+        $text_arr = str_split($text);                       //string -> array
+        $key = count($letters) - $key;
+        for($i = 0, $count = count($text_arr); $i < $count; $i++){
+            for($j = 0, $jcount = count($letters); $j < $jcount; $j++){
+                if ($text_arr[$i] == ' '){
+                    $decoder[$i] = ' ';
+                } 
+                if ($text_arr[$i] == $letters[$j]){
+                    if($j < $jcount - $key){
+                        $decoder[$i] = $letters[$j + $key];
+                    }
+                    elseif($j >= $jcount - $key){
+                        $new_key = $j + $key - $jcount;
+                        $decoder[$i] = $letters[$new_key];
+                    }
+                } 
+            }    
+        }
+        return $decoder;
+    }
 
-    // $text = 'London is a capital of Great Britain XyZ';
-    // $key = 3;                                               //offset
-    // $key_word = 'Tatiana';                                  //offset
-    // $encoderText = encoder($text, $key);
-    // $encoder_word = encoder_word($text, $key_word);
-    // echo implode($encoderText)."<hr>";                             //array -> string
-    // echo implode($encoder_word);                        //array -> string
+    function decoder_word($text, $key_word){
+        global $letters;
+        $key_word = strtolower($key_word);                  // text to lower  
+        $key_word = str_split($key_word);                   //string -> array
+
+        $text = strtolower($text);                          // text to lower                    
+        $text_arr = str_split($text);                       //string -> array
+
+        for($m = 0, $mcount = count($key_word); $m < $mcount; $m++){ 
+            for($j = 0, $jcount = count($letters); $j < $jcount; $j++){
+                if ($key_word[$m] == $letters[$j]){
+                    $m_key2[$m] = $j;                          //numbers array
+                }
+            }
+        }
+        for($i = 0, $count = count($text_arr); $i < $count; $i++){
+            for($j = 0, $jcount = count($letters); $j < $jcount; $j++){
+                if ($text_arr[$i] == $letters[$j]){
+                    $m_key1[$i] = $j;
+                }
+            }  
+        }
+        $c = 0;
+        for($j = 0, $jcount = count($text_arr); $j < $jcount; $j++){
+            if ($text_arr[$j] == ' '){
+                $decoder_word[$j] = ' ';
+            }
+            else{
+                $m = $c % count($m_key2);
+                $x = $m_key1[$j] - $m_key2[$m];
+                if ($x < 0){
+                    $x = count($letters) + $x;
+                }
+                $x_m = $x % count($letters);
+                $decoder_word[$j] = $letters[$x_m];
+                $c++;
+            }
+        }
+        return $decoder_word;
+    }
+    
+    //include('view.php');
+
+     $text = 'London is a capital of Great Britain XyZ';
+     //$text = 'eogloa il a vipvttl hn geett uzigabn qgz';
+
+     $key = 3;                                               //offset
+     $key_word = 'Tatiana';                                  //offset
+     $encoderText = encoder($text, $key);
+     $encoder_word = encoder_word($text, $key_word);
+     $decoder = decoder($text, $key);
+     $decoder_word = decoder_word($text, $key_word);
+
+     echo implode($encoderText)."<hr>";                             //array -> string
+     echo implode($encoder_word)."<hr>";                            
+     echo implode($decoder)."<hr>";                                 
+     echo implode($decoder_word);  
 ?>
